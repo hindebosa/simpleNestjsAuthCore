f94f01dfa4ff7ace84c5b36b80e680dcd72804f7
hindebosa <hindebosa@gmail.com>
2024-07-21 14:06:08 +0200

    added prisma and login controller

---
+++ src/common/services/jwtService.ts
@@ -0,0 +1,33 @@
+import * as jwt from 'jsonwebtoken';
+import { default as config } from '../config';
+import { Injectable } from '@nestjs/common';
+import { User } from '../user/interfaces/user.interface';
+import { PrismaService } from 'src/modules/prisma/prisma.service';
+
+
+@Injectable()
+export class JWTService {
+  constructor(private prisma: PrismaService,) { }
+
+
+  async createToken(email, roles) {
+    const expiresIn = config.jwt.expiresIn,
+      secretOrKey = config.jwt.secretOrKey;
+    const userInfo = { email: email, roles: roles };
+    const token = jwt.sign(userInfo, secretOrKey, { expiresIn });
+    return {
+      expires_in: expiresIn,
+      access_token: token,
+    };
+  }
+
+  async validateUser(signedUser): Promise<User> {
+    var userFromDb = await this.prisma.user.findUnique({ where: { email: signedUser.email } });
+    if (userFromDb) {
+      return userFromDb;
+    }
+    return null;
+  }
+
+
+}
